2450a9252269be0cb48874991e894a01ba4573bb
Caio Vinícius <caio1525pereira@gmail.com>
2024-05-14 23:30:16 -0300

    feat: :sparkles: Rota de autenticação via senha

--- apps/api/src/http/routes/index.ts
+++ apps/api/src/http/routes/index.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from "fastify";
 
+import { authenticateWithPassword } from "./auth/authenticate-with-password";
 import { createAccount } from "./auth/create-account";
 
 export async function routes(app: FastifyInstance) {
   app.register(createAccount);
+  app.register(authenticateWithPassword);
 }
