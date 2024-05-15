292a20682ead2566947b83ed9dc714684d849866
Caio Vinícius <caio1525pereira@gmail.com>
2024-05-14 21:17:42 -0300

    feat: :sparkles: Rota de cadastro de usuário

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from "bcryptjs";
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+
+import { prisma } from "@/lib/prisma";
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,17 +17,32 @@ export async function createAccount(app: FastifyInstance) {
         })
       }
     },
-    (request, reply) => {
+    async (request, reply) => {
       const { name, email, password } = request.body;
 
-      return reply.status(201).send({
-        message: "User created!",
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email
+        }
+      });
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          message: "user with same e-mail already exists."
+        });
+      }
+
+      const passwordHash = await hash(password, 8);
+
+      await prisma.user.create({
         data: {
           name,
           email,
-          password
+          passwordHash
         }
       });
+
+      return reply.status(201).send();
     }
   );
 }
