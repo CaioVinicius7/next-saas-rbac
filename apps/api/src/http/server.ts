82f4953d601ac36ab9877c2207cc2118c558f2e2
Caio Vinícius <caio1525pereira@gmail.com>
2024-05-16 19:32:19 -0300

    feat: :goal_net: Error Handling

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -10,12 +10,15 @@ import {
   type ZodTypeProvider
 } from "fastify-type-provider-zod";
 
+import { errorHandler } from "./error-handler";
 import { routes } from "./routes";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.setErrorHandler(errorHandler);
 
 app.register(fastifySwagger, {
   openapi: {
