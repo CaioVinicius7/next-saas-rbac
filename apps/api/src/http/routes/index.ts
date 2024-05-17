45e9e09530ced164b0dbccd20ad7eb84a996ed10
Caio Vinícius <caio1525pereira@gmail.com>
2024-05-16 21:09:41 -0300

    feat: :sparkles: Rota de solicitação de troca de senha

--- apps/api/src/http/routes/index.ts
+++ apps/api/src/http/routes/index.ts
@@ -3,9 +3,11 @@ import type { FastifyInstance } from "fastify";
 import { authenticateWithPassword } from "./auth/authenticate-with-password";
 import { createAccount } from "./auth/create-account";
 import { getProfile } from "./auth/get-profile";
+import { requestPasswordRecover } from "./auth/request-password-recover";
 
 export async function routes(app: FastifyInstance) {
   app.register(createAccount);
   app.register(authenticateWithPassword);
   app.register(getProfile);
+  app.register(requestPasswordRecover);
 }
