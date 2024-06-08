f8ef0611a7ab8d87fcdab64d1ad50884446d2cf1
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-08 20:31:26 -0300

    feat: :tada: Setup inicial do projeto web

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "SaaS",
+  description: "SaaS"
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
