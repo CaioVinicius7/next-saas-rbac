a277aac003aa05de97d99e89836daeed9a02cafe
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-12 18:31:57 -0300

    feat: :lipstick: Página de recuperação de senha

---
+++ apps/web/src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function ForgotPasswordPage() {
+  return (
+    <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input id="email" name="email" type="email" autoComplete="off" />
+      </div>
+
+      <Button type="submit" className="w-full">
+        Recovery password
+      </Button>
+
+      <Button type="submit" variant="link" size="sm" className="w-full" asChild>
+        <Link href="/auth/sign-in">Sign in instead</Link>
+      </Button>
+    </form>
+  );
+}
