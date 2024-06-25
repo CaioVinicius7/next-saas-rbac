99977514766d5b60a6aac17a4696efbd3bcafca9
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-25 16:52:00 -0300

    feat: :lipstick: Componente: OrganizationSwitcher

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from "lucide-react";
 import Image from "next/image";
 
 import logo from "@/assets/logo.svg";
 
+import { OrganizationSwitcher } from "./OrganizationSwitcher";
 import { ProfileButton } from "./ProfileButton";
 
 export function Header() {
@@ -9,6 +11,10 @@ export function Header() {
     <header className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={logo} alt="logo" className="size-6 dark:invert" />
+
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
