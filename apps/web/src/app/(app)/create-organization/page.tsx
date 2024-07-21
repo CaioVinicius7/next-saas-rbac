a65ab6173fd70b76f6e1451a3aad92b112819c8d
Caio Vinícius <caio1525pereira@gmail.com>
2024-07-21 15:02:34 -0300

    refactor: :lipstick: Alteração no layout

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,10 +1,17 @@
+import { Header } from "@/components/Header";
+
 import { OrganizationForm } from "./OrganizationForm";
 
 export default function CreateOrganization() {
   return (
-    <div className="space-y-4">
-      <h1 className="text-2xl font-bold">Create organization</h1>
-      <OrganizationForm />
+    <div className="space-y-4 py-2">
+      <Header />
+
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <h1 className="text-2xl font-bold">Create organization</h1>
+
+        <OrganizationForm />
+      </main>
     </div>
   );
 }
--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
 
 import { isAuthenticated } from "@/auth/auth";
-import { Header } from "@/components/Header";
 
 export default function AuthLayout({
   children
@@ -13,11 +12,5 @@ export default function AuthLayout({
     redirect("/auth/sign-in");
   }
 
-  return (
-    <div className="space-y-4 py-4">
-      <Header />
-
-      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
-    </div>
-  );
+  return <>{children}</>;
 }
