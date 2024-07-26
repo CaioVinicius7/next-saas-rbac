d64a5584a8ac2df3b3ea488805cff13525a70331
Caio Vinícius <caio1525pereira@gmail.com>
2024-07-25 21:10:14 -0300

    feat: :sparkles: Parallel routes

---
+++ apps/web/src/app/(app)/@test/create-organization/page.tsx
@@ -0,0 +1,3 @@
+export default function CreateOrganization() {
+  return <h1>Parallel route - Create organization</h1>;
+}
--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -4,13 +4,20 @@ import type { ReactNode } from "react";
 import { isAuthenticated } from "@/auth/auth";
 
 export default function AuthLayout({
-  children
+  children,
+  test
 }: Readonly<{
   children: ReactNode;
+  test: ReactNode;
 }>) {
   if (!isAuthenticated()) {
     redirect("/auth/sign-in");
   }
 
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      {test}
+    </>
+  );
 }
